59978b3309c2fdd7e5aba1a1e0d292aca7459814
Souparna Dhara <189111059+souparnad2004@users.noreply.github.com>
2026-05-03 22:52:24 +0530

    Add keys

--- src/common/utils/jose.ts
+++ src/common/utils/jose.ts
@@ -1,15 +1,13 @@
-import { importPKCS8, importSPKI } from "jose";
 import fs from "node:fs/promises";
-import path from "path";
 
-const privateKey = await importPKCS8(
-  await fs.readFile(path.join(process.cwd(), "keys/private-key.pem"), "utf-8"),
-  "RS256"
+const privateKey = await fs.readFile(
+  "/etc/secrets/private-key.pem",
+  "utf-8"
 );
 
-const publicKey = await importSPKI(
-  await fs.readFile(path.join(process.cwd(), "keys/public-key.pem"), "utf-8"),
-  "RS256"
+const publicKey = await fs.readFile(
+  "/etc/secrets/public-key.pem",
+  "utf-8"
 );
 
-export { privateKey, publicKey };
+export {privateKey, publicKey}
